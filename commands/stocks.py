b11f94ecdadb63c680845686ee41442548bdd922
Xavierdarkness <mrb1105@gmail.com>
2021-06-22 08:59:08 -0400

    changed everything in stocks.py

--- commands/stocks.py
+++ commands/stocks.py
@@ -4,9 +4,10 @@ from discord.ext import commands
 from discord.ext.commands import bot
 import requests
 import logging
+from datetime import datetime
 import aiohttp
 import asyncio
-from os import environ
+from os import environ, name
 
 
 class stocks(commands.Cog, name="stocks"):
@@ -14,37 +15,67 @@ class stocks(commands.Cog, name="stocks"):
         self.bot = bot
         self.token = environ['STOCK_API_KEY']
 
-    @commands.command(name='stocks', help='responds with stock data')
+    @commands.command(name='stocks', help='Use +stocks and the name of the company, i.e, +stocks GME.')
     async def news(self, context, symbol):
         # replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
         url = 'https://www.alphavantage.co/query'
         params = {
-            f"function": "TIME_SERIES_INTRADAY",
-            f"symbol": {symbol},  # remove later
-            f'interval': '5min',
+            f"function": "GLOBAL_QUOTE",
+            f"symbol": symbol,  # remove later
+            # f'interval': '5min',
             f'apikey': self.token
         }
         async with aiohttp.ClientSession() as session:
             async with session.get(url, params=params) as response:
                 if response.status == 200:
-                    print(response)
                     stonks = await response.json()
-                    print(pp.pformat(stonks))
-                    for stonk in stonks['Meta Data'].items():
-                        embed = discord.Embed(
-                            title=f'Information'
-                        )
-                        embed.add_field(
-                            name='Symbol', value=f'**{stonk[:][1]}**', inline=False
-                        )
-                        for data in stonks['Time Series (5min)'].items():
-                            embed.add_field(
-                                name='Description', value=f'**{data[:][1]}**', inline=False
-                            )
+                    symbol = stonks["Global Quote"]["01. symbol"]
+                    open = stonks["Global Quote"]["02. open"]
+                    high = stonks["Global Quote"]["03. high"]
+                    low = stonks["Global Quote"]["04. low"]
+                    price = stonks["Global Quote"]["05. price"]
+                    volume = stonks['Global Quote']["06. volume"]
+                    last_day = stonks["Global Quote"]["07. latest trading day"]
+                    previous_close = stonks["Global Quote"]["08. previous close"]
+                    change = stonks["Global Quote"]["09. change"]
+                    change_percent = stonks["Global Quote"]["10. change percent"]
+                    embed = discord.Embed(
+                    )
 
-                            await context.send(embed=embed)
+                    embed.add_field(
+                        name='symbol',  value=f"**{symbol}**"
+                    )
+
+                    embed.add_field(
+                        name='open', value=f"**{open}**")
+
+                    embed.add_field(
+                        name='high', value=f"**{high}**")
+
+                    embed.add_field(
+                        name='low', value=f"**{low}**")
+
+                    embed.add_field(
+                        name='price', value=f"**{price}**")
+
+                    embed.add_field(
+                        name='volume', value=f"**{volume}**")
+
+                    embed.add_field(
+                        name='latest trading day', value=f"**{last_day}**")
+
+                    embed.add_field(
+                        name='previous close', value=f"**{previous_close}**")
+
+                    embed.add_field(
+                        name='change', value=f"**{change}**")
+
+                    embed.add_field(
+                        name='change percent', value=f"**{change_percent}**")
+
+                    await context.reply(embed=embed)
                 else:
-                    await context.send(f'No articles found!')
+                    await context.send(f'No company found!')
 
 
 def setup(bot):
